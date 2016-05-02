5a9f0107b877d5a0d823e4efaf6d4b185a8efe9a
Dale Chen <dale@verecho.com>
2016-05-02 12:52:13 -0700

    Update install.rb

--- remote_syslog2/recipes/install.rb
+++ remote_syslog2/recipes/install.rb
@@ -2,7 +2,7 @@ install = node['remote_syslog2']['install']
 bin_file = "#{install['bin_path']}/#{install['bin']}"
 
 remote_file install['download_path'] do
-  Chef::Log.info("Installing remote_syslog2...")
+  Chef::Log.info("Installing remote_syslog2 from #{install['download_path'}...")
   source install['download_file']
   mode '0644'
 #  not_if { ::File.exists?(bin_file) }
@@ -14,8 +14,8 @@ bash 'extract remote_syslog2' do
     mkdir -p #{install['extracted_path']}
     tar xzf #{install['download_path']} -C #{install['extract_path']}
     mv #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
+    rm -rf #{install['download_path']} #{install['extracted_path']}
   EOH
-#    rm -rf #{install['download_path']} #{install['extracted_path']}
 
 Chef::Log.info("Extract remote_syslog2...")
 #  not_if { ::File.exists?(bin_file) }
